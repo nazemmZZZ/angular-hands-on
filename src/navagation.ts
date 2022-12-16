3bfc8aab7e37000b42d2533de0b173e49aed030f
shabasha5 <shabasha1@pubilcsapaient.com>
2022-12-16 16:01:09 +0530

    final commit

--- src/navagation.ts
+++ src/navagation.ts
@@ -7,17 +7,23 @@ export interface RoutesToUrl{
   name: string;
   url: string
   loginReq: LoginReq;
-  active: Boolean
+  active: boolean
 }
 
 export const routesToUrl: RoutesToUrl[] = [
   { name: 'Home', url: '/', loginReq: LoginReq.COMMON, active: true },
-  { name: 'Register', url: '/register', loginReq: LoginReq.NO, active: false },
+  { name: 'Register', url: '/', loginReq: LoginReq.NO, active: false },
   { name: 'Login', url: '/login', loginReq: LoginReq.NO, active: false },
   { name: 'Logout', url: '/logout', loginReq: LoginReq.YES, active: false },
   {
     name: 'Cart',
     url: '/products/cart',
+    loginReq: LoginReq.YES,
+    active: false,
+  },
+  {
+    name: 'Search',
+    url: '/products/search',
     loginReq: LoginReq.YES,
     active: false,
   },
